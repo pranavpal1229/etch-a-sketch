1d62049cc94caa5dc0ef8d10901247b6decb47f3
pranavpal <pranavpal@uchicago.edu>
2024-02-24 14:32:04 -0600

    got the hover functionality

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
 let slider = document.querySelector('.slider')
 let size_text = document.querySelector('#size-text')
+initialBoard()
+size_text.innerHTML = `Grid Size: ${slider.value} x ${slider.value}`
+slider.oninput = function(){
+    clearBoard()
+    size_text.innerHTML = `Grid Size: ${slider.value} x ${slider.value}`
+}
 
-slider.addEventListener()  
+
+
+function clearBoard(){
+    list_of_pieces = Array.from(document.getElementsByClassName('piece'))
+    list_of_pieces.forEach(element => {
+        element.backgroundColor = 'white'
+    });
+}
+
+
+
+
+
+
+function initialBoard(){
+    let board = document.querySelector(".main-screen")
+    let squareSize = Number(550/slider.value)
+    console.log(squareSize)
+    for(let i = 0; i < 550; i += squareSize){
+        let column = document.createElement('div')
+        column.style.width = `${squareSize}px`
+        column.style.height = '450px';
+        for(let j = 0; j < 470; j+= squareSize){
+            let row_piece = document.createElement('div')
+            row_piece.style.width = `${squareSize}px`
+            row_piece.style.height = `${squareSize}px`
+            row_piece.className = 'piece'
+            row_piece.addEventListener("mouseover", function(){
+                row_piece.style.backgroundColor = 'black'
+            })
+            column.appendChild(row_piece)
+        }
+        board.appendChild(column)
+    }
+    
+}
+
+
